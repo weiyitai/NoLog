3c71abf8937155c6e070a0ae0eaccdc6679c4bb5
weiyitai <qianbajin@gmail.com>
2021-09-12 19:51:47 +0800

    增加可乐影视

--- app/src/main/java/com/qianbajin/nn/h/HookKeLe.java
+++ app/src/main/java/com/qianbajin/nn/h/HookKeLe.java
@@ -1,9 +1,16 @@
 package com.qianbajin.nn.h;
 
-import android.app.AndroidAppHelper;
+import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.text.format.DateUtils;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 /**
  * @author qianbajin
  * @date at 2021/9/1 0001  22:46
@@ -11,7 +18,15 @@ import android.text.format.DateUtils;
 public class HookKeLe {
 
     public void hook() {
-        SharedPreferences sp = AndroidAppHelper.currentApplication().getSharedPreferences("config", Context.MODE_PRIVATE);
-        sp.edit().putLong("free_ad_time", System.currentTimeMillis() + DateUtils.DAY_IN_MILLIS).apply();
+        Method onCreate = XposedHelpers.findMethodExact(Application.class, "onCreate");
+        XposedBridge.hookMethod(onCreate, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                SharedPreferences sp = ((Application) param.thisObject).getSharedPreferences("config", Context.MODE_PRIVATE);
+                sp.edit().putLong("free_ad_time", System.currentTimeMillis() + DateUtils.DAY_IN_MILLIS).apply();
+                Log.d("HookKeLe", "HookKeLe success");
+            }
+        });
     }
 }
